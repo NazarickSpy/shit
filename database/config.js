98e89b5b3fabcee4294ea4526c28e6df112774da
NazarickSpy <saominaja@gmail.com>
2025-10-15 05:22:21 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8316634587:AAHPyMl-NW2LZSsmSUyH5b8NU7FvAOUb7mg",  // Ubah Jadi Token Bot Mu !!!
+  owner: "6208011594", // Ubah Jadi Id Mu !!!
+  port: "3043", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "167.172.83.48" // Ubah Jadi Ip Vps Mu !!!
+};
